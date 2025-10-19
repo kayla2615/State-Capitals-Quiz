b989763ee9e485834448493a911d3f35d41e5f33
kayla2615 <kaylamcguinn@gmail.com>
2025-10-19 15:40:25 -0400

    Add QuizActivity and QuizResults with navigation functionality
    - Created QuizActivity.java and QuizResults.java
    - Added corresponding layout files (activity_quiz.xml, activity_quiz_results.xml)
    - Updated MainActivity to handle button navigation (button1 -> QuizActivity, button2 -> QuizResults)
    - Registered new activities in AndroidManifest.xml
    - Added state_capitals.csv file in assets folder with complete state data

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
@@ -1,6 +1,10 @@
 package edu.uga.cs.statecapitalsquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    //button to take users to an activity that starts a new quiz
+    private Button button1;
+    //button to take users to an activity with past quiz results
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        button1 = findViewById( R.id.button );
+        button1.setOnClickListener( new ButtonClickListener() );
+
+        button2 = findViewById( R.id.button2 );
+        button2.setOnClickListener( new ButtonClickListener() );
+    }
+
+    private class ButtonClickListener implements
+            View.OnClickListener
+    {
+        @Override
+        public void onClick( View view ) {
+            Intent intent;
+            if (view.getId() == R.id.button) {
+                // Button1 clicked
+                intent = new Intent( view.getContext(), QuizActivity.class );
+            } else {
+                // Button2 clicked
+                intent = new Intent( view.getContext(), QuizResults.class );
+            } 
+            startActivity( intent );
+        }
     }
 }
+
+
